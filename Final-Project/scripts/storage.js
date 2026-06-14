433f95a78408f72ed0aea7c4efacb6116fa2d482
emmanuelbassey685 <emmanuelbassey685@yahoo.com>
2026-06-14 23:35:08 +0300

    Initial Commit

--- Final-Project/scripts/storage.js
+++ Final-Project/scripts/storage.js
@@ -7,3 +7,12 @@ export function updateTimestamp() {
     // Store track record of access
     localStorage.setItem('skycast-last-visit', new Date().toISOString());
 }
+
+export function savePreference(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function getPreference(key, defaultValue) {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : defaultValue;
+}
